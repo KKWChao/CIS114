415698983abcefa993b8a6710bf8b0d72ab8371e
kc <[email protected]>
2025-09-18 02:59:59 -0700

    finished hw3-2

--- js/hw3/hw3-2.js
+++ js/hw3/hw3-2.js
@@ -1,3 +1,10 @@
+import validateInput from "../utils/validateInput.js";
+
+const form = document.getElementById("commisions-form");
+form.addEventListener("submit", function (event) {
+  calcCommisions(event);
+});
+
 function calcCommisions(event) {
   event.preventDefault();
 
@@ -8,23 +15,41 @@ function calcCommisions(event) {
   let amountSold = 0;
   let weeklyEarning = 0;
 
+  let qtArr = new Array(4);
+  let totalArr = new Array(4);
+
+  let error = "";
+
+  // loop to calc values and input to arrays
   for (let i = 1; i <= 4; i++) {
-    let item = document.getElementById(`item${i}`);
-    let quantity = document.getElementById(`qtyItem${i}`);
-    let itemTotal = document.getElementById(`totalItem${i}`);
+    let userInput = document.getElementById(`item${i}`).value;
+    error += validateInput(userInput, `Item ${i}`);
 
-    let quantityVal = parseInt(item.value);
-    let totalVal = parseFloat((item.value * values[i - 1]).toFixed(2));
+    if (error === "") {
+      qtArr[i - 1] = parseInt(userInput);
+      totalArr[i - 1] = parseFloat((userInput * values[i - 1]).toFixed(2));
 
-    quantity.innerHTML = quantityVal;
-    itemTotal.innerHTML = `$${totalVal}`;
-
-    amountSold += quantityVal;
-    weeklyEarning += totalVal;
+      amountSold += qtArr[i - 1];
+      weeklyEarning += totalArr[i - 1];
+    }
   }
 
-  totalAmountSold.innerHTML = `$${weeklyEarning.toFixed(2)}`;
-  totalWeeklyEarning.innerHTML = `$${parseFloat(
-    weeklyEarning * 0.09 + 250
-  ).toFixed(2)}`;
+  if (error === "") {
+    for (let i = 1; i <= 4; i++) {
+      let quantity = document.getElementById(`qtyItem${i}`);
+      let itemTotal = document.getElementById(`totalItem${i}`);
+
+      quantity.innerHTML = qtArr[i - 1];
+      itemTotal.innerHTML = `$${totalArr[i - 1]}`;
+    }
+
+    totalAmountSold.innerHTML = `$${weeklyEarning.toFixed(2)}`;
+    totalWeeklyEarning.innerHTML = `$${parseFloat(
+      weeklyEarning * 0.09 + 250
+    ).toFixed(2)}`;
+  } else {
+    let errorBox = document.getElementById("error-box");
+
+    errorBox.innerHTML = error;
+  }
 }
--- js/utils/validateInput.js
+++ js/utils/validateInput.js
@@ -1,8 +1,8 @@
 export default function validateInput(input, label) {
-  if (input.trim() === "") {
+  if (input === "") {
     return `${label} is missing!<br>`;
   } else if (parseInt(input) < 0 || parseInt(input) > 100) {
-    return `${label} is an invalid input`;
+    return `${label} is an invalid input<br>`;
   } else if (isNaN(parseInt(input))) {
     return `${label} is a string!<br>`;
   }
