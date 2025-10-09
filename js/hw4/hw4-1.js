713053595d63e7d86a1207074670f9432c2d1758
kc <[email protected]>
2025-10-08 17:45:35 -0700

    fixing mistakes on part1

--- js/hw4/hw4-1.js
+++ js/hw4/hw4-1.js
@@ -5,7 +5,8 @@ let secondGroupProd = document.getElementById("3rd-prod");
 let secondGroupSum = document.getElementById("3rd-sum");
 
 // Arr of range {5 ... 25}
-const numArr = Array.from({ length: 21 }, (_, i) => i + 5);
+const numArr1 = Array.from({ length: 21 }, (_, i) => i + 5);
+const numArr2 = Array.from({ length: 18 }, (_, i) => i + 3);
 
 function forStringOutputBuilder(arr, operator, result) {
   let string = result != null ? "The result of " : "";
@@ -25,13 +26,13 @@ let sum = 0;
 
 let newArr = [];
 
-for (let i = 0; i < numArr.length; i += 4) {
-  prod *= numArr[i];
-  sum += numArr[i];
-  newArr.push(numArr[i]);
+for (let i = 0; i < numArr1.length; i += 4) {
+  prod *= numArr1[i];
+  sum += numArr1[i];
+  newArr.push(numArr1[i]);
 }
 
-stringArr.innerHTML = forStringOutputBuilder(numArr, " ");
+// stringArr.innerHTML = forStringOutputBuilder(numArr, " ");
 
 firstGroupProd.innerHTML = forStringOutputBuilder(
   newArr,
@@ -50,10 +51,10 @@ sum = 0;
 newArr = [];
 
 let j = 0;
-while (j < numArr.length) {
-  prod *= numArr[j];
-  sum += numArr[j];
-  newArr.push(numArr[j]);
+while (j < numArr2.length) {
+  prod *= numArr2[j];
+  sum += numArr2[j];
+  newArr.push(numArr2[j]);
   j += 3;
 }
 
